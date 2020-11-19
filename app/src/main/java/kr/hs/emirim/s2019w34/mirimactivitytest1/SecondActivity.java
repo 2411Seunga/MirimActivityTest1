257b5c3df585409f35d52038672c1a493dca50c8
LeeSeungA <seunga4170@gmail.com>
2020-11-19 20:30:37 +0900

    Complete ActivityTest1

---
+++ app/src/main/java/kr/hs/emirim/s2019w34/mirimactivitytest1/SecondActivity.java
@@ -0,0 +1,26 @@
+package kr.hs.emirim.s2019w34.mirimactivitytest1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Button btnGoBack = findViewById(R.id.btn_goback);
+        btnGoBack.setOnClickListener(gobackListener);
+    }
+
+    View.OnClickListener gobackListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            finish();
+        }
+    };
+}
